813bfd7fb07668bda2a9b48953c4e36749436f4f
Apoorv Prasad <apoorvgupta958@gmail.com>
2025-12-06 05:23:02 +0000

    variable update

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+ const accountId = 1444553
+ let accountEmail = "apoorv@google.com"
+ var accountPassword ="12345"
+ accountCity ="bhopal"
+ let accuntState;
+
+//  accountId=2 // not allowed
+
+accountEmail ="manb@xn.com"
+accountPassword ="054220"
+accountCity = "bengaluru"
+ console.log(accountId);
+
+ /*
+ prefer not to use var
+ becouse of issue in block scope and function scope
+ */
+
+ console.table([accountId,accountEmail,accountPassword,accountCity,accuntState])
