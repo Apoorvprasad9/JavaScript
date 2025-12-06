095209d8f8eb51715de9dda6ec1d38090a5e9f0b
Apoorv Prasad <apoorvgupta958@gmail.com>
2025-12-06 14:38:37 +0000

    datatypes summary

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,33 @@
+ //primitive
+
+ //7 types : String ,Number , Boolearn, null,undefiend, Symbol,BigInt
+
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn =false
+const outsideTemp =null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id==anotherId);
+
+const bigNumber=255254423558n
+
+
+
+ // Reference (non primitive)
+
+ // Array, object , Function 
+
+ const hero =["shaktiman", "naagraj", "doga"]
+  let myobj= {
+    name:"apoorv",
+    age : 21,
+ }
+
+  const myFunction =function(){
+    console.log("Hello world");
+  }
