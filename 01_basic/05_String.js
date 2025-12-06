f26f78b1b8e8041206f175936e8ec0cb3c4915ce
Apoorv Prasad <apoorvgupta958@gmail.com>
2025-12-06 15:17:33 +0000

    string

---
+++ 01_basic/05_String.js
@@ -0,0 +1,19 @@
+ const name="apoorv"
+ const repoCount =50
+
+ console.log(name +repoCount +"Value");
+
+ //console.log(name +repoCount +"Value");
+
+ console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+
+ const gameName =new String('apoorv')
+
+ console.log(gameName[0]);
+ console.log(gameName._proto_);
+
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2));
+ console.log(gameName.indexOf('t'));
+ 
