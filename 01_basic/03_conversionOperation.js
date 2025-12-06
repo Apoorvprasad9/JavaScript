d43caf28a890d28055988fe07a24ac31ad942dff
Apoorv Prasad <apoorvgupta958@gmail.com>
2025-12-06 06:24:30 +0000

    convarsion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+ let score = "33"
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+let valueInNumder= Number(score)
+console.log(typeof valueInNumder);
+console.log(valueInNumder);
+
+ //"33"=>33
+ // "33abc"=>NaN
+ // true =>1; false =>0
+
+ let isLoggedIn=1
+ let boolenIsLoggedIn = Boolean(isLoggedIn)
+ console.log(boolenIsLoggedIn);
+
+ // 1=> true; 0 =>false
+ //""=>false
+ // "apoorv"=>true
+ 
+ let someNumber =33
+ let stringNumber =String(someNumber)
+ console.log(stringNumber)
+ console.log( typeof stringNumber)
