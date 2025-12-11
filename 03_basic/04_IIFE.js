f318aa80778f24cbadbcee3ecdee60c81f95a671
Apoorv Prasad <apoorvgupta958@gmail.com>
2025-12-11 18:25:54 +0000

    IIFE

---
+++ 03_basic/04_IIFE.js
@@ -0,0 +1,11 @@
+ //Immediately Invoked Function Expression (IIFE)
+
+ ((function chai(){
+    //named IIFE
+    console.log("Hello World");
+ })()
+   
+ ((name)=>{
+    console.log('DBCONNECTED TWO ${name}');
+    
+ })("Apoorv"))
