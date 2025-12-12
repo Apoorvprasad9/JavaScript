9c7d5f2d8aedc74869a31b2d9b9e36162ce3cd18
Apoorv Prasad <apoorvgupta958@gmail.com>
2025-12-12 18:29:21 +0000

    loop

---
+++ 05_loops/four.js
@@ -0,0 +1,15 @@
+ const myObject={
+    js:'javascript',
+    cpp:'c++',
+    rb:"reby",
+    swift:"swift by apple"
+ }
+
+ for (const key in myObject) {
+    console.log('${key} shortcut is for ${myobject[key]}');
+
+ }
+ const programming =["js","rb","py","java","cpp"]
+ for(const key in programming){
+    console.log(key);
+ }
