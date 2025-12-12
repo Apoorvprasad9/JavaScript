9ecc22603bb84565c830a2c3b6746ed9d2ce74e7
Apoorv Prasad <apoorvgupta958@gmail.com>
2025-12-12 18:14:03 +0000

    for of loop

---
+++ 05_loops/three.js
@@ -0,0 +1,33 @@
+ //for of
+ [" "," "," "]
+ [{},{},{}]
+
+ const arr=[1,2,3,4,5]
+ for(const num of arr){
+    // console.log(num);
+    
+ }
+
+ const greeting ="hello world!"
+ for(const greet of greeting){
+    // console.log('Each char is ${greet}');
+ }
+
+ //Maps
+
+ const map =new Map()
+ map.set('IN',"India")
+ map.set('USE',"united States of America")
+ map.set('Fr',"France")
+ map.set('IN',"India")
+
+ console.log(map);
+
+ for(const [key,value] of map){
+   console.log(key,':-',value);
+ }
+
+ const myobject ={
+   'game1':'NFS',
+   'game2':'spiderman'
+ }
