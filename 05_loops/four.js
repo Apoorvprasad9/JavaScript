7b2eca7795f466c1ef086d8d7973c0b47162a647
Apoorv Prasad <apoorvgupta958@gmail.com>
2025-12-13 16:48:43 +0000

    for in loop

--- 05_loops/four.js
+++ 05_loops/four.js
@@ -11,5 +11,14 @@
  }
  const programming =["js","rb","py","java","cpp"]
  for(const key in programming){
-    console.log(key);
+    console.log(programming[key]);
  }
+const map =new Map()
+map.set('IN',"India")
+map.set('USA',"united States of Americe")
+map.set('Fr',"Frence")
+map.set('IN',"India")
+
+for(const key in Object){
+   console.log(key);
+}
